b183a4ce3baf3be21106e15d070d65977e3fc0eb
Fer619 <fbriceno@ucol.mx>
2020-11-27 11:52:08 -0600

    implementation of es multiplo

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 export default class App {
 
     esPar(numero){
-    
+    let result;
         if(numero === 0){
             return false;
         } else if(numero % 2 === 0){
@@ -9,11 +9,25 @@ export default class App {
         } else{
             return false;
         }
+        return result;
+    }
+    esMultiplo(numero1, numero2){
+        let result;
+
+        if(numero1 % numero2 === 0){
+            return true;
+        }else {
+            return false;
+        }
+        return result;
     }
 }
 
 let app = new App();
 
-console. log (app.esPar(0));
-console. log (app.esPar(3));
-console. log (app.esPar(6));
+console. log(app.esPar(0));
+console. log(app.esPar(3));
+console. log(app.esPar(6));
+
+console. log(app.esMultiplo(10,2));
+console. log(app.esMultiplo(10,6));
