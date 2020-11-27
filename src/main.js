7da2c63bcbf48713f0afb87cea4d720db4aec36e
Fer619 <fbriceno@ucol.mx>
2020-11-27 12:15:30 -0600

    im++

--- src/main.js
+++ src/main.js
@@ -21,6 +21,24 @@ export default class App {
         }
         return result;
     }
+    calcularSalario(tipoTrabajador, horasTrabajadas){
+        let result;
+
+        switch(tipoTrabajador){
+            case "1":
+                result = 20 * horasTrabajadas;
+                break;
+            case "2":
+                result = 22 * horasTrabajadas;
+                break;
+            case "3":
+                result = 25 * horasTrabajadas;
+                break;
+            default:
+                result = -1
+        }
+        return result;
+    }
 }
 
 let app = new App();
@@ -31,3 +49,8 @@ console. log(app.esPar(6));
 
 console. log(app.esMultiplo(10,2));
 console. log(app.esMultiplo(10,6));
+
+console. log(app.calcularSalario("1"));
+console. log(app.calcularSalario("2"));
+console. log(app.calcularSalario("3"));
+console. log(app.calcularSalario("4"));
